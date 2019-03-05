2166f6abea6bff49f35361574314278a717bffb7
Rishabh Kalakoti <rishabh.kalakoti@gmail.com>
2019-03-05 16:55:10 +0530

    1 added

--- asst6/1.java
+++ asst6/1.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 class Gen extends Thread
 {	
 	// find divisors
+	Object lock = new Object();
 	int num;
 	int sum = 0;
 	Gen(int n)
@@ -10,30 +11,62 @@ class Gen extends Thread
 	}
 	public void run()
 	{
-		wait();
-		for(int i=2;i<num;i++)
+		synchronized(this)
 		{
-			if(num%i==0)
-				sum+=i;
+			try
+			{
+				for(int i=2;i<num;i++)
+				{
+					if(num%i==0)
+						sum+=i;
+				}
+				wait();
+				Thread.sleep(100);
+				notify();
+			}
+			catch(InterruptedException e)
+			{
+				e.printStackTrace();
+			}
+			
 		}
-		notify();
 	}
 	public int getSum()
 	{
+		synchronized(this)
+		{
+			try
+			{
+				notify();
+				wait();
+			}
+			catch(InterruptedException e)
+			{
+				e.printStackTrace();
+			}
+		}
 		return sum;
+		
 	}
 }
 class AmicableNumber
 {
 	public static void main(String[] args)
 	{
-		Scanner sc = new Scanner();
+		Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
 		int b = sc.nextInt();
-		gen csa = new gen(a);
-		gen csb = new gen(b);
+		Gen csa = new Gen(a);
+		Gen csb = new Gen(b);
 		csa.start();
 		csb.start();
-		
+		if(csa.getSum() == csb.getSum())
+		{
+			System.out.println("Amicable numbers.");
+		}
+		else
+		{
+			System.out.println("Not amicable numbers.");
+		}
 	}
 }
